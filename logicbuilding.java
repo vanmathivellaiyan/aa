6f8383e7cf11e617b431f73a4da8a5905a78288e
vanmathivellaiyan <vanmathivellaiyan1995@gmail.com>
2016-07-27 13:48:33 +0530

    Add files via upload

---
+++ logicbuilding.java
@@ -0,0 +1,10 @@
+package test;
+
+public class logicbuilding {
+	public static void main(String[] args){
+		StringBuffer str=new StringBuffer("hello");
+		str.reverse();
+		System.out.println(str);
+	}
+
+}
